46ebdafbd5115460b7159dc880b3e7c0c917513f
tombellens <tombellens96@hotmail.com>
2024-09-27 23:07:20 +0200

    added menu bar, changed header

--- app.js
+++ app.js
@@ -11,3 +11,12 @@ function toggleProject(projectNumber) {
     }
 }
 
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
