6aaf7e7ecd614d9a8509a24c4a4fe9bca2b3ad15
tombellens <tombellens96@hotmail.com>
2024-09-27 22:20:05 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+function toggleProject(projectNumber) {
+    const details = document.getElementById(`project${projectNumber}-details`);
+    const shortText = document.getElementById(`project${projectNumber}-short`);
+
+    if (details.style.display === "block") {
+        details.style.display = "none";
+        shortText.style.display = "block";
+    } else {
+        details.style.display = "block";
+        shortText.style.display = "none";
+    }
+}
+
